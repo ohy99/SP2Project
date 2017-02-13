d9db4f63848f43f87d0c99bff94fc102c33775ab
unknown <160324T@M314G603.sidm.student.local>
2017-02-13 14:47:03 +0800

    Updated RangeWeapon.h & RangeWeapon.cpp

--- Application/Source/RangeWeapon.cpp
+++ Application/Source/RangeWeapon.cpp
@@ -1,6 +1,45 @@
 #include "RangeWeapon.h"
 
-void RangeWeapon::setWeaponDamage(int damage)
+// TODO when player picks up ammo on the floor, chest or whatever, just call this function
+void RangeWeapon::setWeaponAmmo(int totalAmmo)
 {
+	this->totalAmmo = this->totalAmmo + totalAmmo;
+}
 
+// TODO when reloading the gun, just call this function
+void RangeWeapon::setGunAmmo()
+{
+	if (totalAmmo >= maxAmmo && ammo < maxAmmo) 
+	{
+		totalAmmo += ammo;
+		ammo -= ammo;
+		ammo = maxAmmo;
+		totalAmmo -= maxAmmo;
+	}
+	else if (totalAmmo < maxAmmo && ammo < maxAmmo)
+	{
+		totalAmmo = totalAmmo + ammo;
+		ammo -= ammo;
+
+		if (totalAmmo >= maxAmmo)
+		{
+			ammo = maxAmmo;
+			totalAmmo -= maxAmmo;
+		}
+		else
+		{
+			ammo = totalAmmo;
+			totalAmmo -= totalAmmo;
+		}
+	}
+}
+
+int RangeWeapon::getWeaponAmmo()
+{
+	return totalAmmo;
+}
+
+int RangeWeapon::getGunAmmo()
+{
+	return ammo;
 }
